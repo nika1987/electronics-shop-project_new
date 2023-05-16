da08e361234cb1b2704a1906a710535bd802598e
Veronika <veronika.apakova@mail.ru>
2023-05-16 20:53:26 +0300

    create project №2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,19 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        if len(value) <= 10:
+            self.__name = value
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +43,22 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @staticmethod
+    def string_to_number(value: str):
+        return int(value)
+
+    @classmethod
+    def instantiate_from_csv(cls, filename):
+        """
+        класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_
+        """
+        with open(filename) as f:
+            reader = csv.DictReader(f)
+            cls.all.clear()
+            for row in reader:
+                name = row['name']
+                price = cls.string_to_number(row['price'])
+                quantity = cls.string_to_number(row['quantity'])
+                cls.all.append(cls(name, price, quantity))  # создаем экзмляры классы и кладем их в список
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -7,6 +7,12 @@ from src.item import Item
 def item():
     return Item("test_item", 10.0, 5)
 
+@pytest.fixture
+def items_csv():
+    Item.instantiate_from_csv("src/items.csv")
+    return Item.all
+
+
 def test_calculate_total_price(item):
     assert item.calculate_total_price() == 50.0
 
@@ -18,6 +24,17 @@ def test_apply_discount(item):
 def test_all_items():
     item1 = Item("item1", 5.0, 10)
     item2 = Item("item2", 7.0, 8)
-    assert len(Item.all) == 2
-    assert Item.all[0].name == "item1"
-    assert Item.all[1].name == "item2"
+    assert len(Item.all) == 4
+    assert Item.all[2].name == "item1"
+    assert Item.all[3].name == "item2"
+
+
+def test_instantiate_from_csv(items_csv):
+    assert len(items_csv) == 10
+    assert isinstance(items_csv[0], Item)
+    assert items_csv[0].name == "Смартфон"
+    assert items_csv[0].price == 100
+    assert items_csv[0].quantity == 1
+    assert items_csv[2].name == "Ноутбук"
+    assert items_csv[2].price == 1000
+    assert items_csv[2].quantity == 3
